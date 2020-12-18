ebbb4f8ec7c2f6794cf693ea9a39e2aa9b796786
alex5317 <72842981+alex5317@users.noreply.github.com>
2020-12-18 13:26:38 -0800

    Update Check if the time series is statinary by ADF test.R

--- R/Check if the time series is statinary by ADF test.R
+++ R/Check if the time series is statinary by ADF test.R
@@ -8,7 +8,7 @@
 library(quantmod)
 library(aTSA)
 
-adf test = function(){
+adf.test = function(){
   quantmod::getSymbols("WTI", from="2015-04-26", to="2019-10-09")
   WTIAdj = unclass(WTI$WTI.Adjusted)
   adf.test(diff(WTIAdj))
